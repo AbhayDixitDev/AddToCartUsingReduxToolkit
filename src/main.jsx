4e3e39ebb8b1d8ada7e426a6ba312bf5b8812b6b
Abhay Dixit <dixitshivam534@gmail.com>
2024-12-28 23:57:20 +0530

    cart functionality using redux toolkit and redux persisit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
-import store from './store'
-import {Provider} from 'react-redux'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store} >
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
   </Provider>,
 )
